ca701b7064298398180dfb492353dfaf179e2ba8
Marcin Zajaczkowski <szpak@users.sourceforge.net>
2014-09-08 13:10:44 +0200

    Basic orders update

--- src/main/java/pl/warsjawa/legacycode/PaymentServlet.java
+++ src/main/java/pl/warsjawa/legacycode/PaymentServlet.java
@@ -7,10 +7,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Legacy servlet to handle changes in payments.
- *
+ * <p>
  * WARNING. This is legacy code for education purposes only. <b>DO NOT WRITE CODE LIKE THAT AT HOME!</br>
  */
 public class PaymentServlet extends HttpServlet {
@@ -27,25 +29,37 @@ public class PaymentServlet extends HttpServlet {
             Class.forName("org.h2.Driver");
             Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost:9092//tmp/payments", "prod", "topsecret");
 
-            String id = req.getParameter("id");
-            String status = req.getParameter("status");
+            String pDesc = req.getParameter("desc");
+            String pAmount = req.getParameter("amount");
+            String pStatus = req.getParameter("status");
 
-            Statement statement = conn.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM payments WHERE id = " + id);
+            if (pStatus.equals("OK")) {
+                Pattern orderPattern = Pattern.compile("Order: (\\d\\d\\d\\d\\d)");
+                Matcher matcher = orderPattern.matcher(pDesc);
+                if (matcher.matches()) {
+                    String orderId = matcher.group(1);
+                    System.out.println("Retrieved orderId: " + orderId);
 
-            if (resultSet.next()) {
-                String oldStatus = resultSet.getString("status");
-                if (oldStatus.equals(status)) {
-                    responseText = "THE SAME";
-                } else if (oldStatus.equals("NEW") && status.equals("PAID")) {
-                    responseText = "PAID";
-                } else if (oldStatus.equals("PAID") && status.equals("NEW")) {
-                    responseText = "ERROR";
+                    responseText = doIt(responseText, conn, pStatus, orderId, pAmount);
                 } else {
-                    responseText = "ERROR";
-                }
-            }
+                    orderPattern = Pattern.compile(".*(\\d\\d\\d\\d\\d).*");
+                    matcher = orderPattern.matcher(pDesc);
+                    if (matcher.matches()) {
+                        String orderId = matcher.group(1);
+                        System.out.println("Greedily retrieved orderId: " + orderId);
 
+                        responseText = doIt(responseText, conn, pStatus, orderId, pAmount);
+                    } else {
+                        System.out.println("Unrecognized payment description");
+                        responseText = "ERROR";
+                        //TODO: WORKSHOP: Send mail to admin
+                    }
+                }
+
+            } else {
+                System.out.println("Wrong status");
+                responseText = "ERROR";
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
@@ -55,5 +69,50 @@ public class PaymentServlet extends HttpServlet {
         PrintWriter writer = resp.getWriter();
         writer.println(responseText);
         writer.close();
+    }
+
+    //TODO: WORKSHOP: Inline it to make it even worse (with some subtle modification in one branch to make refactoring harder)
+    private String doIt(String responseText, Connection conn, String pStatus, String orderId, String pAmount) throws SQLException {
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM orders WHERE orderId = " + orderId);
+
+        if (resultSet.next()) {
+            String oldStatus = resultSet.getString("status");
+            if (oldStatus.equals("PAID")) {
+                System.out.println("Already paid. Ignoring.");
+                responseText = "ALREADY PAID";
+            } else if (oldStatus.equals("NEW") && pStatus.equals("OK")) {
+                if (new Integer(pAmount) == resultSet.getInt("amount")) {
+                    System.out.println("Paid");
+                    responseText = "PAID";
+                    Statement updateStatement = conn.createStatement();
+                    updateStatement.executeUpdate("UPDATE orders SET status = 'PAID' WHERE orderId = " + orderId);
+                    //TODO: WORKSHOP: Send email to user
+                } else if (new Integer(pAmount) < resultSet.getInt("amount")) {
+                    System.out.println("Not enough");
+                    responseText = "NOT ENOUGH";
+                    //TODO: WORKSHOP: Send email to user
+                } else {
+                    System.out.println("Paid with surplus");
+                    responseText = "PAID SURPLUS";
+                    Statement updateStatement = conn.createStatement();
+                    updateStatement.executeUpdate("UPDATE orders SET status = 'PAID' WHERE orderId = " + orderId);
+                    //TODO: WORKSHOP: Send email to user
+                    //TODO: Handle surplus manually
+                }
+            } else if (oldStatus.equals("CANCELLED") && pStatus.equals("OK")) {
+                System.out.println("Wrong transition error");
+                responseText = "ERROR";
+            } else {
+                System.out.println("Other error");
+                responseText = "ERROR";
+                //TODO: WORKSHOP: Send email to admin
+            }
+        } else {
+            System.out.println("Unknown order");
+            responseText = "ERROR";
+            //TODO: WORKSHOP: Send email to admin
+        }
+        return responseText;
     }
 }
--- src/test/resources/initProdDB.sql
+++ src/test/resources/initProdDB.sql
@@ -1,4 +1,5 @@
-DROP TABLE IF EXISTS payments;
-CREATE TABLE payments(id bigint auto_increment, status varchar(255));
-INSERT INTO payments(status) VALUES ('NEW');
-INSERT INTO payments(status) VALUES ('PAID');
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders(id bigint auto_increment, orderId VARCHAR(255), product VARCHAR(255), amount INT, status varchar(255), email VARCHAR(255));
+INSERT INTO orders(orderId, product, amount, status, email) VALUES ('10001', 'Clean Code book', 3000, 'NEW', 'foo@example.com');
+INSERT INTO orders(orderId, product, amount, status, email) VALUES ('10002', 'Working Effectively with Legacy Code', 4200, 'NEW', 'bar@example.com');
+INSERT INTO orders(orderId, product, amount, status, email) VALUES ('10003', 'TDD by example', 2500, 'NEW', 'tdd@example.com');
