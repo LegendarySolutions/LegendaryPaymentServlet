bf778586abfab1db3426bdf449a73f76af2a7766
mpi <mpi.michal.piotrkowski+gh@gmail.com>
2014-09-22 21:57:34 +0200

    rewrite characterization tests to expose SignatureValidators behaviour

--- src/test/java/legacycode/PaymentServletTest.java
+++ src/test/java/legacycode/PaymentServletTest.java
@@ -1,79 +1,64 @@
 package legacycode;
 
-import static org.mockito.Mockito.verify;
-
-import javax.servlet.http.HttpServletResponse;
-
-import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PaymentServletTest {
 
-    @Mock
-    private HttpServletResponse response;
-
-    private PaymentServlet servlet;
-
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    
     private long currentTime = 0;
+
+    private SignatureValidator validator;
 
     @Before
     public void setUp() {
         
-        servlet = new PaymentServlet(null, new SignatureValidator(){
+        validator = new SignatureValidator(){
             
             @Override
             protected long currentTime() {
                 return currentTime;
             }
-        });
-    }
-
-    @After
-    public void verifyNoMoreInteractions(){
-        Mockito.verifyNoMoreInteractions(response);
-    }
-    
-    @Test
-    public void characterization_test_1() throws Exception {
-
-        servlet.handle(response, "", "", "", "", "");
-
-        verify(response).sendError(HttpServletResponse.SC_FORBIDDEN, "MD5 signature do not match!");
+        };
     }
 
     @Test
-    public void characterization_test_2() throws Exception {
+    public void shouldFailIfInvalidSignature() throws Exception {
 
-        servlet.handle(response, "", "", "", "100000", "5f142f02085b27c938897385782563f6");
+        thrown.expectMessage("MD5 signature do not match!");
 
-        verify(response).sendError(HttpServletResponse.SC_FORBIDDEN, "Timestamp do not match!");
+        validator.assertValidRequest("", "", "", "", "");
     }
 
     @Test
-    public void characterization_test_3() throws Exception {
+    public void shouldFailIfInvalidTimestamp() throws Exception {
+
+        thrown.expectMessage("Timestamp do not match!");
+
+        validator.assertValidRequest("", "", "", "100000", "5f142f02085b27c938897385782563f6");
+    }
+
+    @Test
+    public void shouldPassIfValidSignatureAndTimestamp() throws Exception {
         
         currentTime = 100000;
         
-        servlet.handle(response, "", "", "", "100000", "5f142f02085b27c938897385782563f6");
-        
-        verify(response).sendError(HttpServletResponse.SC_BAD_REQUEST, "Unrecognized format of payload!");
+        validator.assertValidRequest("", "", "", "100000", "5f142f02085b27c938897385782563f6");
     }
     
     @Test
-    public void characterization_test_bug() throws Exception {
+    public void shouldPadShortMd5SignatureWithZeros() throws Exception {
         
         currentTime = 1411390522346L;
         
-        servlet.handle(response, "10000", "OK", "order_id:6792", "1411390522344", "01b931f2c4aa0b0bc24e18c88eeec21b");
-        
-        verify(response).sendError(HttpServletResponse.SC_BAD_REQUEST, "No pending oder with id: 6792!");
-        verify(response, Mockito.never()).sendError(HttpServletResponse.SC_FORBIDDEN, "MD5 signature do not match!");
+        validator.assertValidRequest("10000", "OK", "order_id:6792", "1411390522344", "01b931f2c4aa0b0bc24e18c88eeec21b");
     }
     
 }
