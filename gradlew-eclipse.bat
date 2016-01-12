5c0096f885a0dde90a73767559a35dd54022ae43
Marcin Zajaczkowski <szpak@users.sourceforge.net>
2016-01-13 00:07:03 +0100

    Add gradlew-eclipse.bat
    For Windows people who do not like using console.

---
+++ gradlew-eclipse.bat
@@ -0,0 +1,10 @@
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows generating Eclipse configuration
+@rem
+@rem  Created as one-click solution for people who don't like console
+@rem
+@rem ##########################################################################
+
+call gradlew.bat eclipse
