55072b835f05a74a0ac9a91cd8baac620885d0c7
Marcin Zajączkowski <szpak@users.sourceforge.net>
2019-05-24 10:44:26 +0200

    [MD5] Reproduce bug with test

--- src/test/java/legacycode/PaymentServletTest.java
+++ src/test/java/legacycode/PaymentServletTest.java
@@ -79,4 +79,17 @@ public class PaymentServletTest implements WithAssertions {
         //then
         then(response).should().sendError(HttpServletResponse.SC_BAD_REQUEST, "Unrecognized format of payload!");
     }
+
+    @Test
+    //https://github.com/LegendarySolutions/LegendaryPaymentServlet/issues/1
+    public void shouldReproduceIssueWithMd5() throws IOException {
+        //given
+        //POST http://legacy-solutions.com/api/payments HTTP/1.1 403
+        //amount=10000&status=OK&payload=order_id%3A6792&ts=1411677303294&md5=0c672178b3ce4ddc5404833b94cf5982
+        //when
+        paymentServlet.process(response, "10000", "OK", "order_id:6792",
+                "1411677303294", "0c672178b3ce4ddc5404833b94cf5982");
+        //then
+        verify(response).sendError(HttpServletResponse.SC_FORBIDDEN, "MD5 signature do not match!");
+    }
 }
