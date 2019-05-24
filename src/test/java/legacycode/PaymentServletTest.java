55b044d488281dbec885d9e1f126ce1a43e813c1
Marcin Zajączkowski <szpak@users.sourceforge.net>
2019-05-24 11:32:22 +0200

    [MD5] Fix MD5 calculation algorithm

--- src/test/java/legacycode/PaymentServletTest.java
+++ src/test/java/legacycode/PaymentServletTest.java
@@ -1,6 +1,7 @@
 package legacycode;
 
 import org.assertj.core.api.WithAssertions;
+import org.h2.jdbc.JdbcSQLException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -25,7 +26,10 @@ public class PaymentServletTest implements WithAssertions {
     private HttpServletResponse response;
     @Mock
     private PaymentService paymentService;
+
     private PaymentServlet paymentServlet;
+
+    private long currentTime = 100_000L;
 
     @After
     public void cleanup() {
@@ -37,7 +41,7 @@ public class PaymentServletTest implements WithAssertions {
         paymentServlet = new PaymentServlet(paymentService, new RequestParamValidator() {
             @Override
             protected long currentTime() {
-                return 100_000L;
+                return currentTime;
             }
         });
     }
@@ -81,14 +85,18 @@ public class PaymentServletTest implements WithAssertions {
 
     @Test
     //https://github.com/LegendarySolutions/LegendaryPaymentServlet/issues/1
-    public void shouldReproduceIssueWithMd5() throws IOException {
+    public void shouldKeepLeadingZeroInMd5() throws IOException {
         //given
         //POST http://legacy-solutions.com/api/payments HTTP/1.1 403
         //amount=10000&status=OK&payload=order_id%3A6792&ts=1411677303294&md5=0c672178b3ce4ddc5404833b94cf5982
+        currentTime = 1411677303293L;
         //when
-        paymentServlet.process(response, "10000", "OK", "order_id:6792",
-                "1411677303294", "0c672178b3ce4ddc5404833b94cf5982");
+        Throwable thrown = catchThrowable(() -> paymentServlet.process(response, "10000", "OK", "order_id:6792",
+                "1411677303294", "0c672178b3ce4ddc5404833b94cf5982"));
         //then
-        verify(response).sendError(HttpServletResponse.SC_FORBIDDEN, "MD5 signature do not match!");
+        assertThat(thrown)
+                .isInstanceOf(RuntimeException.class)
+                .hasCauseInstanceOf(JdbcSQLException.class)
+                .hasMessageContaining("[90067-190]");
     }
 }
