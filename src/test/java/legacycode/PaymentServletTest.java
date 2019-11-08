1a0d5cbb862bc9c2cb1534ca213d90e8ade76aa6
Marcin Zajączkowski <szpak@users.sourceforge.net>
2019-11-08 14:11:00 +0100

    [MD5] Base tests

---
+++ src/test/java/legacycode/PaymentServletTest.java
@@ -0,0 +1,69 @@
+package legacycode;
+
+import org.assertj.core.api.WithAssertions;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+
+public class PaymentServletTest implements WithAssertions {
+
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+
+    @Mock
+    private HttpServletResponse response;
+    @Mock
+    private PaymentService paymentService;
+    private PaymentServlet paymentServlet;
+
+    @Before
+    public void init() {
+        paymentServlet = new PaymentServlet(paymentService) {
+            @Override
+            protected long currentTime() {
+                return 1_000_001;
+            }
+        };
+    }
+
+    @After
+    public void cleanup() {
+        verifyNoMoreInteractions(response);
+    }
+
+    @Test
+    public void should1() throws IOException {
+        //when
+        paymentServlet.process(response, "", "", "", "", "");
+        //then
+        verify(response).sendError(HttpServletResponse.SC_FORBIDDEN, "MD5 signature do not match!");
+    }
+
+    @Test
+    public void should2() throws IOException {
+        //when
+        paymentServlet.process(response, "", "", "", "10000", "2d9f2cff82ca49088bc4629bb288dd51");
+        //then
+        verify(response).sendError(HttpServletResponse.SC_FORBIDDEN, "Timestamp do not match!");
+    }
+
+    @Test
+    public void should3() throws IOException {
+        //when
+        paymentServlet.process(response, "", "", "", "1000000", "cfd1b42a1306944e98a49f1ecb7b38e");
+        //then
+        verify(response).sendError(HttpServletResponse.SC_BAD_REQUEST, "Unrecognized format of payload!");
+    }
+
+}
