fd96f16d718992e2dd1b428d52322591a1d56184
Marcin Zajączkowski <szpak@users.sourceforge.net>
2019-11-20 14:10:24 +0100

    [MD5] Extract validation to separate class

--- src/test/java/legacycode/PaymentServletTest.java
+++ src/test/java/legacycode/PaymentServletTest.java
@@ -32,12 +32,12 @@ public class PaymentServletTest implements WithAssertions {
 
     @Before
     public void init() {
-        paymentServlet = new PaymentServlet(paymentService) {
+        paymentServlet = new PaymentServlet(paymentService, new RequestValidator() {
             @Override
             protected long currentTime() {
                 return currentTime;
             }
-        };
+        });
     }
 
     @After
