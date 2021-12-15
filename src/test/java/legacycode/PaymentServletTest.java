a044db710eecb775fdaa72d95f9274b789d9adf6
Marcin Zajączkowski <148013+szpak@users.noreply.github.com>
2021-12-15 14:41:47 +0100

    [MD5] Base tests

--- src/test/java/legacycode/PaymentServletTest.java
+++ src/test/java/legacycode/PaymentServletTest.java
@@ -29,7 +29,12 @@ public class PaymentServletTest implements WithAssertions {
 
     @Before
     public void setup() {
-        paymentServlet = new PaymentServlet(paymentService);
+        paymentServlet = new PaymentServlet(paymentService) {
+            @Override
+            protected long currentTime() {
+                return 100_000L;
+            }
+        };
     }
 
     @After
@@ -52,6 +57,22 @@ public class PaymentServletTest implements WithAssertions {
                 paymentServlet.process(response, "", "", "", "", "ba76a036471586d9417a0cee2fc78ee2"));
         //then
         assertThat(thrown).isInstanceOf(NumberFormatException.class);
+    }
+
+    @Test
+    public void should3() throws IOException {
+        //when
+        paymentServlet.process(response, "", "", "", "10000", "2d9f2cff82ca49088bc4629bb288dd51");
+        //then
+        verify(response).sendError(HttpServletResponse.SC_FORBIDDEN, "Timestamp do not match!");
+    }
+
+    @Test
+    public void should4() throws IOException {
+        //when
+        paymentServlet.process(response, "", "", "", "100001", "2303ad9f2ef11e722cfa7da72308803c");
+        //then
+        verify(response).sendError(HttpServletResponse.SC_BAD_REQUEST, "Unrecognized format of payload!");
     }
 
 }
