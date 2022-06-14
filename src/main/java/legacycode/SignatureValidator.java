faeb7e606550bc261bf1e627aa8e7f2873f5a4cd
Marcin Zajączkowski <148013+szpak@users.noreply.github.com>
2022-06-14 15:52:27 +0200

    [MD5] Fix MD5 calculation algorithm

--- src/main/java/legacycode/SignatureValidator.java
+++ src/main/java/legacycode/SignatureValidator.java
@@ -1,32 +1,17 @@
 package legacycode;
 
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import org.apache.commons.codec.digest.DigestUtils;
 
 public class SignatureValidator {
 
     private static final String secret = "15c84df6-bfa3-46c1-8929-a5dedaeab4a4";
 
     void validateSignature(String amount, String status, String payload, String timestamp, String md5) {
-        try {
+        String expectedMd5 = DigestUtils.md5Hex(amount + status + payload + timestamp + secret);
+        System.out.println("Expected MD5: " + expectedMd5);
 
-            MessageDigest digest = MessageDigest.getInstance("MD5");
-            digest.update(amount.getBytes());
-            digest.update(status.getBytes());
-            digest.update(payload.getBytes());
-            digest.update(timestamp.getBytes());
-            digest.update(secret.getBytes());
-
-            String expectedMd5 = String.format("%x", new BigInteger(1, digest.digest()));
-            System.out.println("Expected MD5: " + expectedMd5);
-
-            if (!expectedMd5.equals(md5)) {
-                throw new ValidationException("MD5 signature do not match!");
-            }
-
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
+        if (!expectedMd5.equals(md5)) {
+            throw new ValidationException("MD5 signature do not match!");
         }
 
         if (Math.abs(currentTime() - Long.valueOf(timestamp)) > 60000) {
