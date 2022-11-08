c826fc83e582ce79ad1e3999a7fb33ea83d876f5
Marcin Zajączkowski <148013+szpak@users.noreply.github.com>
2022-11-08 13:50:24 +0100

    [MD5] Reproduce bug with test

--- src/test/java/legacycode/PaymentServletTest.java
+++ src/test/java/legacycode/PaymentServletTest.java
@@ -24,12 +24,14 @@ public class PaymentServletTest implements WithAssertions {
 
     private PaymentServlet paymentServlet;
 
+    private long currentTime = 100_000L;
+
     @Before
     public void init() {
         paymentServlet = new PaymentServlet(paymentService) {
             @Override
             protected long currentTime() {
-                return 100_000L;
+                return currentTime;
             }
         };
     }
@@ -69,6 +71,18 @@ public class PaymentServletTest implements WithAssertions {
         paymentServlet.process(response, "", "", "", "100001", "2303ad9f2ef11e722cfa7da72308803c");
         //then
         verify(response).sendError(HttpServletResponse.SC_BAD_REQUEST, "Unrecognized format of payload!");
+    }
+
+    @Test
+    public void shouldReproduceProblemWithMD5() throws IOException {
+        //given
+        //POST http://legacy-solutions.com/api/payments HTTP/1.1 403
+        //amount=10000&status=OK&payload=order_id%3A6792&ts=1411677303294&md5=0c672178b3ce4ddc5404833b94cf5982
+        currentTime = 1411677303293L;
+        //when
+        paymentServlet.process(response, "10000", "OK", "order_id:6792", "1411677303294", "0c672178b3ce4ddc5404833b94cf5982");
+        //then
+        verify(response).sendError(HttpServletResponse.SC_FORBIDDEN, "MD5 signature do not match!");
     }
 
 }
