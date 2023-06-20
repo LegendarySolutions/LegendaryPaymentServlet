2d55ff67c3a02541e3a87c53a5a0fa28826c30a9
Marcin Zajączkowski <148013+szpak@users.noreply.github.com>
2023-06-20 11:48:24 +0200

    [MD5] Fix MD5 calculation algorithm

--- src/main/java/legacycode/RequestValidator.java
+++ src/main/java/legacycode/RequestValidator.java
@@ -1,33 +1,21 @@
 package legacycode;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 import java.io.Serializable;
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class RequestValidator implements Serializable {
     private static final String secret = "15c84df6-bfa3-46c1-8929-a5dedaeab4a4";
 
     void validate(String amount, String status, String payload, String timestamp, String md5) {
-        try {
 
-            MessageDigest digest = MessageDigest.getInstance("MD5");
-            digest.update(amount.getBytes());
-            digest.update(status.getBytes());
-            digest.update(payload.getBytes());
-            digest.update(timestamp.getBytes());
-            digest.update(secret.getBytes());
+        String expectedMd5 = DigestUtils.md5Hex(amount + status + payload + timestamp + secret);
+        System.out.println("Expected MD5: " + expectedMd5);
 
-            String expectedMd5 = String.format("%x", new BigInteger(1, digest.digest()));
-            System.out.println("Expected MD5: " + expectedMd5);
-
-            if (!expectedMd5.equals(md5)) {
-                throw new ValidationException("MD5 signature do not match!");
-            }
-
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
+        if (!expectedMd5.equals(md5)) {
+            throw new ValidationException("MD5 signature do not match!");
         }
+
 
         if (Math.abs(currentTime() - Long.valueOf(timestamp)) > 60000) {
             throw new ValidationException("Timestamp do not match!");
